ee92d3bdf22e5af7189f9d0a211e592c91012c31
Максим Полозков <pipl82308@gmail.com>
2025-07-04 18:56:09 +0700

    Update

--- src/masks.py
+++ src/masks.py
@@ -1,15 +1,27 @@
 from typing import Union
 
+import logging
+
+logger = logging.getLogger(__name__)
+
+file_handler = logging.FileHandler('../log/masks.log', "w", encoding="utf-8")
+file_formatter = logging.Formatter('%(asctime)s %(filename)s %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.setLevel(logging.INFO)
+
 
 def get_mask_card_number(card_number: Union[str, int]) -> str:
     """Функция для шифрования номера карты"""
     card_number_str = str(card_number)
 
-    if card_number_str == " ":
+    if card_number_str == "":
+        logger.info('Поле для ввода пустое')
         return "Поле для ввода пустое"
     elif card_number_str.isalpha():
+        logger.info('Номер карты должен состоять только из цифр')
         return "Номер карты должен состоять только из цифр"
     elif len(card_number_str) > 15:
+        logger.info('Номер карты не должен быть больше 15 цифр')
         return "Номер карты не должен быть больше 15 цифр"
 
     # Получаем первые 6 цифр
@@ -28,10 +40,9 @@ def get_mask_card_number(card_number: Union[str, int]) -> str:
     return formatted_number
 
 
-# if __name__ == '__main__':
-
-# card_number = str(input())
-# print(get_mask_card_number(card_number))
+if __name__ == '__main__':
+    card = str(input())
+    print(get_mask_card_number(card))
 
 
 def get_mask_account(account_number: Union[int, str]) -> str:
@@ -39,10 +50,13 @@ def get_mask_account(account_number: Union[int, str]) -> str:
     account_number_str = str(account_number)  # Преобразуем в строку
 
     if account_number_str == " ":
+        logger.info('Поле для ввода пустое')
         return "Поле для ввода пустое"
     elif account_number_str.isalpha():
+        logger.info('Номер счета должен состоять только из цифр')
         return "Номер счета должен состоять только из цифр"
     elif len(account_number_str) < 19:
+        logger.info('Номер счета должен быть больше 19 цифр')
         return "Номер счета должен быть больше 19 цифр"
 
     # Получаем последние 4 цифры
